84f11486640ce51c4277c17c9b0c05c5f9e6ad20
Riccardo Musto <42040779+notprime@users.noreply.github.com>
2022-06-16 11:44:07 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -274,3 +274,46 @@ def evaluate_accuracy_gpu(net, data_iter, device = None):
       y = y.to(device)
       metric.add(tools.accuracy(net(X), y), y.numel())
   return metric[0] / metric[1]
+
+def train_on_gpu(net, train_iter, test_iter, num_epochs, lr, device):
+  def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+      nn.init.xavier_uniform_(m.weight)
+  net.apply(init_weights) # apply xavier initialization
+  print("Training on: ", device)
+  net.to(device)
+  optimizer = torch.optim.SGD(net.parameters(), lr = lr)
+  loss = nn.CrossEntropyLoss()
+  animator = tools.Animator(xlabel = 'epoch', xlim = [1, num_epochs],
+                            legend = ['train loss', 'train acc', 'test acc'])
+  timer = tools.Timer()
+  num_batches = len(train_iter)
+  for epoch in range(num_epochs):
+    metric = tools.Accumulator(3)
+    net.train()
+    for i, (X, y) in enumerate(train_iter):
+      timer.start()
+      optimizer.zero_grad()
+      X, y = X.to(device), y.to(device)
+      y_hat = net(X)
+      l = loss(y_hat, y)
+      l.backward()
+      optimizer.step()
+      with torch.no_grad():
+        metric.add(l * X.shape[0], tools.accuracy(y_hat, y), X.shape[0])
+      timer.stop()
+      train_l = metric[0] / metric[2]
+      train_acc = metric[1] / metric[2]
+      if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+        animator.add(epoch + (i + 1) / num_batches,
+                     (train_l, train_acc, None))
+    test_acc = evaluate_accuracy_gpu(net, test_iter)
+    animator.add(epoch + 1, (None, None, test_acc))
+  print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, test acc {test_acc:.3f}')
+  #print(f'{metric[2] * num_epochs / timer.cumsum():.1f} examples/sec on {str(device)}')
+
+def try_gpu(i = 0):
+  """ Return gpu(i) if exists, otherwise return cpu() """
+  if torch.cuda.device_count() >= i + 1:
+    return torch.device(f'cuda:{i}')
+  return torch.device('cpu')
